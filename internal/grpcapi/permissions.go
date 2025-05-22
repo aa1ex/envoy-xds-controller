9b19a93f82b25c81355a3d39c7a8196947e98b0d
Aleksandr Aleksandrov <aaleksandrov.cy@gmail.com>
2025-05-22 17:54:17 +0300

    fix bug: get permissions

--- internal/grpcapi/permissions.go
+++ internal/grpcapi/permissions.go
@@ -1,10 +1,11 @@
 package grpcapi
 
 import (
-	"context"
-
 	"connectrpc.com/connect"
+	"context"
+	"fmt"
 	"github.com/casbin/casbin/v2"
+	"github.com/casbin/casbin/v2/constant"
 	v1 "github.com/kaasops/envoy-xds-controller/pkg/api/grpc/permissions/v1"
 	"github.com/kaasops/envoy-xds-controller/pkg/api/grpc/permissions/v1/permissionsv1connect"
 )
@@ -23,7 +24,8 @@ func (p *PermissionsService) ListPermissions(ctx context.Context, req *connect.R
 	permissions := make(map[string]map[string]struct{})
 	var permItems []*v1.PermissionsItem
 	for _, sub := range authorizer.GetSubjects() {
-		result := p.e.GetPermissionsForUserInDomain(sub, req.Msg.AccessGroup)
+		result, _ := getPermissionsForUserInDomain(p.e, sub, req.Msg.AccessGroup)
+		//result := p.e.GetPermissionsForUserInDomain(sub, req.Msg.AccessGroup)
 		if len(result) > 0 {
 			for _, perm := range result {
 				if len(perm) == 4 {
@@ -47,4 +49,49 @@ func (p *PermissionsService) ListPermissions(ctx context.Context, req *connect.R
 		}
 	}
 	return connect.NewResponse(&v1.ListPermissionsResponse{Items: permItems}), nil
+}
+
+func getPermissionsForUserInDomain(e *casbin.Enforcer, user string, domain string) ([][]string, error) {
+	gtype := "g"
+	ptype := "p"
+
+	permission := make([][]string, 0)
+	rm := e.GetNamedRoleManager(gtype)
+	if rm == nil {
+		return nil, fmt.Errorf("role manager %s is not initialized", gtype)
+	}
+
+	roles, err := e.GetNamedImplicitRolesForUser(gtype, user, domain)
+	if err != nil {
+		return nil, err
+	}
+	policyRoles := make(map[string]struct{}, len(roles)+1)
+	policyRoles[user] = struct{}{}
+	for _, r := range roles {
+		policyRoles[r] = struct{}{}
+	}
+
+	domainIndex, err := e.GetFieldIndex(ptype, constant.DomainIndex)
+	for _, rule := range e.GetModel()["p"][ptype].Policy {
+		ruleDomain := rule[domainIndex]
+		if ruleDomain != "*" {
+			matched := rm.Match(domain, ruleDomain)
+			if !matched {
+				continue
+			}
+		}
+		policyRole := rule[0]
+		if _, ok := policyRoles[policyRole]; ok {
+			newRule := deepCopyPolicy(rule)
+			newRule[domainIndex] = domain
+			permission = append(permission, newRule)
+		}
+	}
+	return permission, nil
+}
+
+func deepCopyPolicy(src []string) []string {
+	newRule := make([]string, len(src))
+	copy(newRule, src)
+	return newRule
 }
